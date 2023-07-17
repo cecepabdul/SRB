b386ec9824b3b75710830d56465e8332467f7ecf
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-07-17 20:37:36 +0700

    Delete maza.sh

--- maza.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER --password $PASSWORD $EXTRAS"
-./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER --password $PASSWORD $EXTRAS
