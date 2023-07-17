e0c737401588233553913bcfcb42618ca7d6abc6
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-07-17 20:33:46 +0700

    Update maza.sh

--- maza.sh
+++ maza.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool stratum+tcp://fastpool.xyz:6000 --wallet solo:MLegX9RugBiRMpJjmENLZEtnoPtGrF7o1hx --password x
+echo "SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER --password $PASSWORD $EXTRAS"
+./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER --password $PASSWORD $EXTRAS
