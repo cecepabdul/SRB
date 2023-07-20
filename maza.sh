d538ca9686c144affbbcc0dda7967fd241d2c7d9
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-07-20 11:14:58 +0700

    Update maza.sh

--- maza.sh
+++ maza.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool stratum+tcp://fastpool.xyz:6000 --wallet solo:MLegX9RugBiRMpJjmENLZEtnoPtGrF7o1hx --password x
+./SRBMiner-MULTI --disable-gpu --algorithm yespowertide  --pool stratum+tcp://stratum-eu.rplant.xyz:7059 --wallet TY38vT7Ydk6cqcKTq8BNkZLyMJtN9wbNLM.dockerS --password webpassword=cecepabdu
